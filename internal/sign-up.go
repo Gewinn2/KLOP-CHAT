21d4a07f7f21b64a1a1106e88bba811b1eac7b91
Gewinn2 <daniilpostnickov@yandex.ru>
2024-05-14 20:20:37 +0300

    Занесение firebase в чушпаны и изменение структуры пользователя

--- internal/sign-up.go
+++ internal/sign-up.go
@@ -1,45 +1,18 @@
 package internal
 
 import (
-	"context"
-	"firebase.google.com/go/v4/auth"
-	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 type User struct {
-	UID      string `json:"uid"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Surname  string `json:"surname"`
+	Id        string `json:"id"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+	UserName  string `json:"user_name"`
+	Photo     string `json:"photo"`
+	CreatedAt string `json:"created_at"`
 }
 
 func HandleSignUp(c *gin.Context) {
-	client, err := InitFirebase().Auth(context.Background())
-	if err != nil {
-		fmt.Println("HandleSignUp:", err)
-	}
 
-	var user User
-
-	err = c.BindJSON(&user)
-	if err != nil {
-		fmt.Println("HandleSignUp:", err)
-	}
-
-	params := (&auth.UserToCreate{}).
-		Email(user.Email).
-		EmailVerified(false).
-		Password(user.Password).
-		DisplayName(user.Name + " " + user.Surname)
-
-	u, err := client.CreateUser(context.Background(), params)
-	if err != nil {
-		fmt.Println("HandleSignUp:", err)
-		c.JSON(http.StatusForbidden, "Пользователь с таким email уже существует")
-	}
-
-	c.JSON(http.StatusCreated, u)
 }
