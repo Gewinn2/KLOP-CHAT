4a3d2e8bbcf478d38dbd7342bbc836d616ef32f9
artificial00 <154523621+artificial00@users.noreply.github.com>
2024-05-16 13:02:28 +0300

    Апдейт

---
+++ internal/chat.go
@@ -0,0 +1,17 @@
+package internal
+
+import(
+	"Astra_Linux_chat/internal/database"
+)
+func (s *Server) createChat(userId1, userId2 int) (int, error) {
+	// Создаем новый чат
+	chat := database.Chat{
+		UserId1: userId1,
+		UserId2: userId2,
+		CreatedAt: "2022-01-01T00:00:00Z",
+	}
+
+	// TODO: надо сохранять чат в бд
+
+	return chat.Id, nil
+}
