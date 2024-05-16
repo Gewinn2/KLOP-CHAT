4b73f226addbdf9c528e0df610f3a52710b7eaf1
Gewinn2 <daniilpostnickov@yandex.ru>
2024-05-16 22:26:03 +0300

    Появилось удаление пользователя по id, изменение пользователя по id, получения всех данных пользователя по id

--- internal/database/user.go
+++ internal/database/user.go
@@ -20,6 +20,7 @@ func CreateUser(db *sql.DB, u User) (int, error) {
 
 func GetUserIdByUsername(db *sql.DB, username string) (User, error) {
 	var user User
+
 	row := db.QueryRow(`SELECT user_id FROM users WHERE username = $1`, username)
 	err := row.Scan(&user.Id)
 	if err != nil {
@@ -31,10 +32,50 @@ func GetUserIdByUsername(db *sql.DB, username string) (User, error) {
 
 func GetUserByEmail(db *sql.DB, email string) (User, error) {
 	var user User
+
 	row := db.QueryRow(`SELECT * FROM users WHERE email = $1`, email)
 	err := row.Scan(&user.Id, &user.Email, &user.Password, &user.UserName, &user.Photo, &user.CreatedAt)
 	if err != nil {
 		return user, fmt.Errorf("DBGetUserByEmail:", err)
 	}
+
 	return user, nil
+}
+
+func GetUserById(db *sql.DB, id int) (User, error) {
+	var user User
+
+	row := db.QueryRow(`SELECT * FROM users WHERE user_id = $1`, id)
+	err := row.Scan(&user.Id, &user.Email, &user.Password, &user.UserName, &user.Photo, &user.CreatedAt)
+	if err != nil {
+		return user, fmt.Errorf("DBGetUserByEmail:", err)
+	}
+
+	return user, nil
+}
+
+func UpdateUserById(db *sql.DB, u User) (User, error) {
+	var user User
+
+	_, err := db.Exec(`UPDATE users 
+		SET username = $1, 
+		    email = $2,
+		    password = $3,
+		    photo = $4
+		WHERE user_id = $5`, user.UserName, user.Email, user.Password, user.Photo, user.Id)
+
+	if err != nil {
+		return user, fmt.Errorf("UpdateUserById:", err)
+	}
+
+	return user, nil
+}
+
+func DeleteUserById(db *sql.DB, id int) error {
+	_, err := db.Exec(`DELETE FROM users WHERE user_id = $1`, id)
+	if err != nil {
+		return fmt.Errorf("DeleteUserById:", err)
+	}
+
+	return nil
 }
