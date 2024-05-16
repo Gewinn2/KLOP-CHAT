958ed137a346132ff139f7e74b1cb2387b0bb6d7
Gewinn2 <daniilpostnickov@yandex.ru>
2024-05-16 22:55:31 +0300

    Добавлена функция создания чата

--- internal/database/chat.go
+++ internal/database/chat.go
@@ -1,8 +1,32 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+)
 
-func CreateChat(db *sql.DB, chat Chat) (Chat, error) {
+func CreateChat(db *sql.DB, chat Chat, userId1, userId2 int) (Chat, error) {
+	var chatID int
+
+	err := db.QueryRow(`INSERT INTO chat(name, photo, created_at)
+		VALUES ($1, $2, $3)
+		RETURNING chat_id`, chat.Name, chat.Photo, chat.CreatedAt).Scan(&chatID)
+	if err != nil {
+		return chat, fmt.Errorf("DBCreateChat:", err)
+	}
+
+	_, err = db.Exec(`INSERT INTO chats_partiсipants (chat_id, user_id)
+		VALUES ($1, $2)`, chatID, userId1)
+	if err != nil {
+		return chat, fmt.Errorf("DBCreateChat:", err)
+	}
+
+	_, err = db.Exec(`INSERT INTO chats_partiсipants (chat_id, user_id)
+		VALUES ($1, $2)`, chatID, userId2)
+	if err != nil {
+		return chat, fmt.Errorf("DBCreateChat:", err)
+	}
+
 	return chat, nil
 }
 
