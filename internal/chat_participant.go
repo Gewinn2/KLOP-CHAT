956befe0dbb1ef79a4e687d8e82b7909e7b3ba72
artificial00 <sonya.yermolina@gmail.com>
2024-05-21 11:28:01 +0300

    Дописаны все функции по добавлению, редактированию чатов, пользователей в чатах, сообщений. + небольшие хотфиксы

---
+++ internal/chat_participant.go
@@ -0,0 +1,20 @@
+package internal
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Функция получения всех пользователей чата по айди чата
+func (s *Server) GetChatParticipants(c *gin.Context) {
+
+}
+
+// Функция добавления пользователя в чат по айди чата и айди пользователя
+func (s *Server) AddParticipant(c *gin.Context) {
+
+}
+
+// Функция удаления пользователя из чата по айди пользователя
+func (s *Server) DeleteParticipant(c *gin.Context) {
+
+}
