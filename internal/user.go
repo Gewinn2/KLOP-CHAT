02e8ce4921d1b226eeee8566cab94b72aba0f9ad
artificial00 <154523621+artificial00@users.noreply.github.com>
2024-05-22 23:20:46 +0300

    Добавлена функция обновления активности

--- internal/user.go
+++ internal/user.go
@@ -26,3 +26,23 @@ func (s *Server) getUserById(c *gin.Context) { // получаем юзера п
 
 	c.JSON(http.StatusOK, user)
 }
+// Функция обновления активности, которая работает по принципу - отправил хттп запрос = онлайн
+func (s *Server) userActivity (c *gin.Context){
+    userIdToConv := c.Param("user_id")
+	userId, _ := strconv.Atoi(userIdToConv)
+
+    // Получаем информацию о пользователе из базы данных
+    var user database.User
+    if _, err := database.GetUserById(s.DB, userId); err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": "Пользователь не найден"})
+        return
+    }
+	// Обновляем время последней активности пользователя в базе данных
+    if err := user.UpdateLastActivity(s.DB); err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Ошибка сервера"})
+        return
+    }
+
+    // Возвращаем дату последней активности в формате "2022-01-000T00:00:00Z"
+    c.JSON(http.StatusOK, gin.H{"last_activity": user.LastActivity})
+}
